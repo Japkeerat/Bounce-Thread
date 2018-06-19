c6cdac3b1387723aa25fd3884d0e90dc18026d0b
Japkeerat Singh <39667771+Japkeerat@users.noreply.github.com>
2018-06-19 10:33:04 +0530

    Add files via upload

---
+++ BounceThread.java
@@ -0,0 +1,18 @@
+package bouncethread;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class BounceThread {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new BounceFrame();
+                frame.setTitle("BounceThread");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
